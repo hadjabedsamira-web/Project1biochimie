89f8beaa39be77e773fca22ae8f28e0fce60816a
NASRI WAIL CHAMS EDDINE <wnasri668@gmail.com>
2025-12-12 16:28:16 +0100

    Correct to_csv method call syntax
    Fix syntax error in to_csv method call.

--- Hadjabedsmaira.py
+++ Hadjabedsmaira.py
@@ -66,4 +66,4 @@ print (df,"\n\n")
 
 #7) Sauvegarde et chargement des données avec pandas
 #Sauvegarder le DataFrame dans un fichier csv
-df.to_csv("tableau_séquence.csv", index=False
+df.to_csv("tableau_séquence.csv"), index=False
