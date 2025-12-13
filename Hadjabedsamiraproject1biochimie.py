25c47db524705e34da3deadd5442865c7f7240cb
hadjabedsamira-web <hadjabedsamira@gmail.com>
2025-12-13 09:18:43 +0100

    Fix formatting issues in Hadjabedsamiraproject1biochimie.py

--- Hadjabedsamiraproject1biochimie.py
+++ Hadjabedsamiraproject1biochimie.py
@@ -14,7 +14,7 @@ import pandas as pd
 data ={
 "Séquence": ["ATGCGTACGTA","GCTAGCTAGGCC","TAGCGCGTAAGT","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
 "Longueur": [12,12,12,10,11,10,10],
- "Pourcentage GC": [50,66.67,58.33,40,45.45,60,50]
+"Pourcentage GC": [50,66.67,58.33,40,45.45,60,50]
  }
 
 
