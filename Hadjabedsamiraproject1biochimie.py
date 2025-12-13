480f83c88145eb2436fe21bb4e6df89609baa583
ghalminoudjoud56-lgtm <ghalminoudjoud56@gmail.com>
2025-12-13 08:36:22 +0100

    Update Hadjabedsamiraproject1biochimie.py

--- Hadjabedsamiraproject1biochimie.py
+++ Hadjabedsamiraproject1biochimie.py
@@ -36,7 +36,7 @@ print(longueur,"\n\n")
 
 #2) Filtrer les séquences dont la longueur supérieure à 10
 print("**************** Filtrage dont la longueur ****************")
-# Filtrer les séquences dont la longueur supérieure à 10 
+# Filtrer les séquences dont la longueurs supérieure à 10 
 filtred_df = df[df["Longueur"] > 10]
 print(filtred_df,"\n\n")
 
